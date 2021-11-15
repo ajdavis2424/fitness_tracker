95dd2975311eed10c9577e134977d8306cdb20d0
ALexander Davis <ajdavis2424@gmail.com>
2021-11-15 16:51:00 -0500

    building out server

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
-// Requires for monggos and express server
+// Requires for mongos and express server
 const express = require("express");
 const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
